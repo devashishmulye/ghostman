ff82a16ba2e1705507921b594cb1ea5a138fbd92
Devashish <devashish@finbox.in>
2018-11-05 11:16:59 +0530

    add dr-run, refactor code

--- remove_friends.py
+++ remove_friends.py
@@ -1,90 +1,138 @@
 from selenium import webdriver
 import time
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 import os
+import argparse
 
 
 
 
-
-driver_path = os.getcwd() + "/chromedriver5"
-driver = webdriver.Chrome(driver_path)
-driver.get("https://www.goodreads.com/")
-time.sleep(10)
-# elem = driver.find_element_by_id('//*[@id="sign_in"]')
-elem = driver.find_element_by_id('userSignInFormEmail')
-elem.send_keys('devashish.mulye@gmail.com')
-elem2 = driver.find_element_by_xpath('//*[@id="user_password"]')
-elem2.send_keys("placeholderpw")
-
-sign_in_elem = driver.find_element_by_xpath('//*[@id="sign_in"]/div[3]/input[1]')
-sign_in_elem.click()
+do_not_delete_list = ['Murtaza Phalasiya','Subhomoy Bakshi','Aaditya Rajawat']
 
 
-time.sleep(10)
-driver.get('https://www.goodreads.com/friend')
+def check_if_friend_in_dnd(friend_name,dnd_list):
+
+    if friend_name in dnd_list:
+        return True
+    else:
+        return False
 
 
-
-do_not_delete_list = ['Murtaza Phalasiya','Subhomoy Bakshi']
-
-friend_number = 4
-for i in range(0,0):
+def sort_by_first_name(driver):
     select = Select(driver.find_element_by_id('sort'))
     select.select_by_visible_text('first name')
-    print 'friend number', friend_number
+    return driver
+
+
+
+
+def execute_deletion(driver,friend_element,is_dry_run):
+    print 'Deleted', friend_element.text
+    deletion_element = friend_element.find_element_by_xpath("../../div[4]/div/a")
+    if is_dry_run is False:
+        deletion_element.click()
+        time.sleep(5)
+        alert_obj = driver.switch_to.alert
+        alert_obj.accept()
+        is_break = True
+    else:
+        is_break = False
+    return is_break
+
+
+def delete_friends_flow(driver, do_not_delete_list,is_dry_run):
+
+    next_page_enabled=True
+
+    while next_page_enabled is True:
+        time.sleep(5)
+        driver = sort_by_first_name(driver)
+        driver = activate_edit_friends(driver)
+        all_friends = driver.find_elements_by_class_name('userLink')
+
+        for friend in all_friends:
+            if check_if_friend_in_dnd(friend.text,do_not_delete_list) is False:
+                is_break = execute_deletion(driver,friend,is_dry_run)
+                if is_break is True:
+                    break
+            else:
+                is_break = False
+                print 'Did Not Delete', friend.text
+        if is_break is True:
+            continue
+
+
+        next_page_element = driver.find_element_by_class_name('next_page')
+        next_page_enabled = next_page_element.get_attribute('href')
+
+        if next_page_enabled is not None:
+            next_page_enabled = True
+            next_page_element.click()
+        else:
+            next_page_enabled = False
+
+
+
+def activate_edit_friends(driver):
     edit_friends_elem = driver.find_element_by_id('edit_friends_button')
     edit_friends_elem.click()
-
-    try:
-        del_friend = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[{}]/div[4]/div'.format(str(friend_number)))
-    except NoSuchElementException:
-        'No Such Element Bruf'
-        break
-
-    elem_with_name = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[{}]/div[1]/a[1]'.format(str(friend_number)))
-    name_of_person = elem_with_name.text
-    print name_of_person, 'Name of Person'
-    if name_of_person in do_not_delete_list:
-        friend_number +=1
-        print 'Did Not Unfriend ', name_of_person
-        continue
-    time.sleep(9)
-    del_friend.click()
-    alert_obj = driver.switch_to.alert
-    alert_obj.accept()
-
-# try:
-#     null_elem = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[4]/div[4]/div/div/div/div/div')
-# except NoSuchElementException:
-#     print 'No Such Element Bruff'
+    return driver
 
 
+def initialize_driver():
+    driver_path = os.getcwd() + "/chromedriver5"
+    driver = webdriver.Chrome(driver_path)
+    return driver
+
+
+def get_goodreads_homepage(driver):
+    driver.get("https://www.goodreads.com/")
+    return driver
+
+def log_in_to_goodreads(driver, email_id, password):
+    sing_in_elem = driver.find_element_by_id('userSignInFormEmail')
+    sing_in_elem.send_keys(email_id)
+    password_elem = driver.find_element_by_xpath('//*[@id="user_password"]')
+    password_elem.send_keys(password)
+
+    sign_in_elem = driver.find_element_by_xpath('//*[@id="sign_in"]/div[3]/input[1]')
+    sign_in_elem.click()
+    return driver
+
+
+def get_goodreads_friends_page(driver):
+    driver.get('https://www.goodreads.com/friend')
+    return driver
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dry-run")
+    parser.add_argument("--email",type=str)
+    parser.add_argument("--password",type=str)
+    parser.add_argument("--retain",type=str)
+    args = parser.parse_args()
+
+    if args.dry_run:
+        print 'This is a Dry Run'
+        is_dry_run = True
+    else:
+        is_dry_run = False
+
+    email = args.email
+    password = args.password
+    do_not_delete_list = args.retain.split(',')
+    print 'These Friends Will Not Be Deleted',do_not_delete_list
+
+    driver = initialize_driver()
+    driver = get_goodreads_homepage(driver)
+    time.sleep(10)
+    driver = log_in_to_goodreads(driver,email,password)
+    driver = get_goodreads_friends_page(driver)
+    time.sleep(10)
+    driver = delete_friends_flow(driver, do_not_delete_list,is_dry_run)
 
 
 
-
-
-'''
-
-
-
-'/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[4]/div[4]/div'
-'orig'
-'/html/body/div[3]/div[3]/div[1]/div[1]/div[3]/div[5]/div[4]/div'
-'/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[4]/div[4]/div/a'
-'/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[4]/div[4]/div/a'
-'/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[4]/div[4]/div'
-
-
-# profile_elem = '/html/body/div[2]/div[1]/div/header/div[1]/div/div[4]/span/a'
-
-'''
-
-el_1_xpath = '/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[4]/div[4]/div'
-el_2_xpath = '/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[5]/div[4]/div'
-el_3_xpath = '/html/body/div[2]/div[3]/div[1]/div[1]/div[3]/div[6]/div[4]/div'
-
-
-
+#python remove_friends.py --email devashish.mulye@gmail.com --password pwpw --retain "Murtaza Phalasiya,Subhomoy Bakshi,Aaditya Rajawat" --dry-run 1
